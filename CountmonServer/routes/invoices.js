340c8046611e71e42e83bb88cbb195614a5c8d65
Simon D <gamedevpuzzler@gmail.com>
2021-03-08 11:42:27 +0100

    Added invoice form submit

--- CountmonServer/routes/invoices.js
+++ CountmonServer/routes/invoices.js
@@ -14,7 +14,7 @@ router.use("/", function (req, res, next) {
         });
 });
 
-router.get("/", function (req, res) {
+router.get("/getIncomingAll", function (req, res) {
     let token = auth.tokenPayload(req.cookies["auth"]);
 
     return db
@@ -23,6 +23,32 @@ router.get("/", function (req, res) {
             res.send(results);
         })
         .catch(function (err) {
+            res.send(err);
+        });
+});
+
+router.post("/addIncoming", function (req, res) {
+    let token = auth.tokenPayload(req.cookies["auth"]);
+    const supplier = req.body.supplier;
+    const invoiceDate = req.body.invoiceDate;
+    const nettosum = req.body.nettosum;
+    const bruttosum = req.body.bruttosum;
+    const tax = bruttosum - nettosum;
+    const file = req.body.file;
+    console.log(file);
+    const isOutgoing = false;
+
+    const valueArray = [token.id, supplier, invoiceDate, nettosum, bruttosum, tax, "", isOutgoing];
+    const invoicesTable = new db.Datatable("invoices");
+
+    return invoicesTable
+        .insert(["userid", "name", "date", "nettosum", "bruttosum", "tax", "filepath", "isoutgoing"], valueArray)
+        .then(function (results) {
+            console.log("Added new invoice: " + valueArray);
+            res.send("Added new invoice: " + valueArray);
+        })
+        .catch(function (err) {
+            console.log(err);
             res.send(err);
         });
 });
